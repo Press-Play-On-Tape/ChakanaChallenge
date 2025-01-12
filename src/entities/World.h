c3d68f993f529e66046540899ff651a94689ed2a
Filmote <simon@bloggingadeadhorse.com>
2025-01-12 22:28:53 +1100

    Save Work

--- src/entities/World.h
+++ src/entities/World.h
@@ -19,8 +19,8 @@ struct World {
         Item items[10];
 
         uint16_t frameCount = 0;
-        uint16_t xMap = 172;
-        uint16_t yMap = 254;
+        uint16_t xMap = 236;
+        uint16_t yMap = 296;
         uint16_t xBoat = 14;
         uint16_t yBoat = 6;
         BoatDirection boatDirection = BoatDirection::None;
@@ -28,8 +28,8 @@ struct World {
         int16_t x = 0;
         int16_t y = 0;
 
-        uint8_t currentPort = 8; //SJH 255
-        uint8_t nextPort = 8; //SJH 255
+        uint8_t currentPort = 255; //SJH 255
+        uint8_t nextPort = 255; //SJH 255
         uint8_t nextPortCost = 0;
         uint16_t portCompleted = 1;
         uint8_t chakanas = 230;
@@ -114,7 +114,7 @@ struct World {
 
         void updateBoat() {
 
-            if (this->frameCount % 8 == 0) {
+            if (this->frameCount % 4 == 0) {
 
                 uint8_t offset = 0;
 
